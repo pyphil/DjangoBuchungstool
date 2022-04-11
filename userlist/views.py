f42f9fce416a840abfd1bee51dd05b50f2bd3131
pyphil <63059088+pyphil@users.noreply.github.com>
2022-04-11 23:28:06 +0200

    success page saves students

--- userlist/views.py
+++ userlist/views.py
@@ -55,12 +55,12 @@ def entry(request):
 
     selection = {
         'short_name': request.POST.get('selection'),
-        # 'datum': selection_query.datum,
-        # 'stunde': selection_query.stunde
+        'datum': selection_query.datum,
+        'stunde': selection_query.stunde
     }
     request.session['short_name'] = request.POST.get('selection')
-    # request.session['datum'] = str(selection_query.datum)
-    # request.session['stunde'] = selection_query.stunde
+    request.session['datum'] = str(selection_query.datum)
+    request.session['stunde'] = selection_query.stunde
 
     numbers = [""]
     for i in range(1, 17):
@@ -89,14 +89,50 @@ def success(request):
     elif request.GET.get('access') and request.session.get('has_access'):
         return redirect('/')
 
-    print(request.POST.get('iPad'))
-    print(request.POST.get('pencil'))
-    print(request.POST.get('students'))
+    ipad = request.POST.get('iPad')
+    pencil = request.POST.get('pencil')
+    students = request.POST.get('students')
 
-    # print(request.session.get('short_name'))
-    # print(request.session.get('datum'))
-    # print(request.session.get('stunde'))
+    save_users_query = Booking.objects.filter(
+        room=request.session.get('short_name'),
+        datum=request.session.get('datum'),
+        stunde=request.session.get('stunde')
+    ).first()
+    print(ipad)
+    if ipad == "01":
+        save_users_query.iPad_01 = pencil + "|" + students
+    if ipad == "02":
+        save_users_query.iPad_02 = pencil + "|" + students
+    if ipad == "03":
+        save_users_query.iPad_03 = pencil + "|" + students
+    if ipad == "04":
+        save_users_query.iPad_04 = pencil + "|" + students
+    if ipad == "05":
+        save_users_query.iPad_05 = pencil + "|" + students
+    if ipad == "06":
+        save_users_query.iPad_06 = pencil + "|" + students
+    if ipad == "07":
+        save_users_query.iPad_07 = pencil + "|" + students
+    if ipad == "08":
+        save_users_query.iPad_08 = pencil + "|" + students
+    if ipad == "09":
+        save_users_query.iPad_09 = pencil + "|" + students
+    if ipad == "10":
+        save_users_query.iPad_10 = pencil + "|" + students
+    if ipad == "11":
+        save_users_query.iPad_11 = pencil + "|" + students
+    if ipad == "12":
+        save_users_query.iPad_12 = pencil + "|" + students
+    if ipad == "13":
+        save_users_query.iPad_13 = pencil + "|" + students
+    if ipad == "14":
+        save_users_query.iPad_14 = pencil + "|" + students
+    if ipad == "15":
+        save_users_query.iPad_15 = pencil + "|" + students
+    if ipad == "16":
+        save_users_query.iPad_16 = pencil + "|" + students
 
+    save_users_query.save()
 
     return render(
         request, 'userlistSuccess.html',
