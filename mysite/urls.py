6fa48917788072a039741e96c32dc81034ff7dc2
pyphil <63059088+pyphil@users.noreply.github.com>
2022-05-24 16:13:04 +0200

    image

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', buchungstoolViews.rooms, name='buchungstoolRooms'),
@@ -34,4 +35,4 @@ urlpatterns = [
     path('tinymce/', include('tinymce.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
