527cf8cb1880d74a3ab0ed7346545a07b2c6aef4
pyphil <63059088+pyphil@users.noreply.github.com>
2023-03-17 17:51:38 +0100

    Update context_processors.py

--- buchungstool/context_processors.py
+++ buchungstool/context_processors.py
@@ -2,8 +2,14 @@ from buchungstool_settings.models import Setting
 
 
 def add_institution(request):
-    obj = Setting.objects.get(name='settings')
-    return {
-        'institution_name': obj.institution,
-        'institution_logo': obj.logo,
-        }
+    try:
+        obj = Setting.objects.get(name='settings')
+        return {
+            'institution_name': obj.institution,
+            'institution_logo': obj.logo,
+            }
+    except Setting.DoesNotExist:
+        return {
+            'institution_name': "",
+            'institution_logo': "",
+            }
