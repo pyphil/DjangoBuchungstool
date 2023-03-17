1af23bb10093a69b7316e8005f3551fde73bdf1c
pyphil <63059088+pyphil@users.noreply.github.com>
2023-03-17 16:39:46 +0100

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,17 +130,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-# Done in production settings on server only
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'public/static')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.joing(BASE_DIR, 'public/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/public/media/'
-MEDIA_ROOT = BASE_DIR / 'public/media'
 
 LOGIN_REDIRECT_URL = "/"
 
